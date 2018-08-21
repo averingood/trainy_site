ae3ede44b801da9fe976358dd12d329e14c0330b
averingood <averingood@gmail.com>
2018-08-21 15:00:38 +0300

    fix outout date on furst page

--- main.js
+++ main.js
@@ -7,4 +7,3 @@ var day = new Array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday
 var mounth = new Array("January","February","March","April","May","June","July","August","September","October","November","December")
 
 headerDate.innerHTML = day[date.getDay() - 1] + ", " + date.getDate() + " " + mounth[date.getMonth()] + ", " + date.getFullYear();
-

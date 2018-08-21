4ddaadfb7f1f37e1a09622c6c3e0183cc0e3cbe0
averingood <averingood@gmail.com>
2018-08-21 17:19:29 +0300

    toggle img on click

--- main.js
+++ main.js
@@ -1,9 +1,52 @@
-var headerDate = document.getElementById('header-date');
 
-var date = new Date();
+function displayDate(id){
 
-var day = new Array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+  var $elem = document.getElementById(id);
 
-var mounth = new Array("January","February","March","April","May","June","July","August","September","October","November","December")
+  var currentDate = new Date();
 
-headerDate.innerHTML = day[date.getDay() - 1] + ", " + date.getDate() + " " + mounth[date.getMonth()] + ", " + date.getFullYear();
+  var day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  var mounth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  var date = day[currentDate.getDay()] + ", " + currentDate.getDate() + " " + mounth[currentDate.getMonth()] + ", " + currentDate.getFullYear();
+
+  $elem.innerHTML = date;
+}
+
+function toggleOkImg() {
+  var $img = document.getElementById('ok-img'),
+      style = getComputedStyle($img),
+      visibility = style.getPropertyValue('visibility');
+
+  if (visibility == 'hidden'){
+    $img.setAttribute('style', 'visibility:visible');
+  } 
+  
+  if (visibility == 'visible'){
+    $img.setAttribute('style', 'visibility:hidden');
+  }
+};
+
+displayDate('header-date');
